da945bb0258b17f607179c752b74932cff9c013d
i-chi-li <i-chi-li@i-chi-li.info>
2020-04-14 16:08:11 +0900

    日時変換サンプルを追加

--- src/main/kotlin/micronaut/kotlin/blanco/sample/DateController.kt
+++ src/main/kotlin/micronaut/kotlin/blanco/sample/DateController.kt
@@ -3,12 +3,10 @@ package micronaut.kotlin.blanco.sample
 import io.micronaut.core.convert.ConversionContext
 import io.micronaut.core.convert.TypeConverter
 import io.micronaut.http.MediaType
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.Produces
-import io.micronaut.http.annotation.QueryValue
+import io.micronaut.http.annotation.*
 import org.slf4j.LoggerFactory
 import java.time.LocalDateTime
+import java.time.OffsetDateTime
 import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
@@ -22,21 +20,39 @@ import javax.inject.Singleton
 class DateController {
     private val log = LoggerFactory.getLogger(this.javaClass)
 
+    /**
+     * 現在時刻を UNIX EPOCH「エポック」（1970 年 1 月 1 日 00:00:00）からのミリ秒で取得する
+     *
+     * curl -i "http://localhost:8080/date/now"
+     */
     @Get("/now")
     @Produces(MediaType.APPLICATION_JSON)
     fun now(): Date {
         return Date()
     }
 
+    /**
+     * LocalDateTimeConverter で、文字列を LocalDateTime に変換するサンプル
+     *
+     * curl -i "http://localhost:8080/date/localDateTime?localDateTime=2020-04-12T10:20:30"
+     */
     @Get("/localDateTime")
     @Produces("${MediaType.APPLICATION_JSON}; ${MediaType.CHARSET_PARAMETER}=utf-8")
     fun localDateTime(@QueryValue localDateTime: LocalDateTime): String {
         return localDateTime.toString()
     }
 
-    @Get("/localDateTime")
+    /**
+     * 値を Bean で受け取る場合のサンプル
+     * Date、 LocalDateTime および、ZonedDateTime フィールドが、変換されるサンプル
+     * LocalDateTimeConverter 、ZonedDateTimeConverter および、ZoneIdConverter で変換される。
+     *
+     * curl -i "http://localhost:8080/date/bean" -X POST -H "Content-Type:application/json" -d '{"date": 1586827230, "localDateTime": "2020-04-14T10:20:30", "zonedDateTime": "2020-04-14T10:20:30+09:00[Asia/Tokyo]", "offsetDateTime": "2020-04-14T10:20:30+09:00", "zoneId": "Asia/Tokyo", "timeZone": "JST"}'
+     */
+    @Post("/bean")
     @Produces("${MediaType.APPLICATION_JSON}; ${MediaType.CHARSET_PARAMETER}=utf-8")
-    fun dateBean(@QueryValue date: DateBean): DateBean {
+    @Consumes(MediaType.APPLICATION_JSON)
+    fun dateBean(@Body date: DateBean): DateBean {
         log.info("data: $date")
         return date
     }
@@ -46,19 +62,33 @@ class DateController {
  * 日時格納用
  */
 class DateBean(
+    // サンプル値「1586846009893」
     val date: Date?,
+    // サンプル値「2020-04-14T10:20:30」
     val localDateTime: LocalDateTime?,
+    // サンプル値「2020-04-14T10:20:30+09:00[Asia/Tokyo]」
     val zonedDateTime: ZonedDateTime?,
+    // サンプル値「2020-04-14T10:20:30+0900」
+    val offsetDateTime: OffsetDateTime?,
+    // サンプル値「Asia/Tokyo」。「JST」はエラーとなる
+    val zoneId: ZoneId?,
+    // サンプル値「JST」
     val timeZone: TimeZone?
 ) {
     fun localToZoned(): ZonedDateTime? {
-        return timeZone?.let {
-            localDateTime?.atZone(it.toZoneId())
+        return zoneId?.let {
+            localDateTime?.atZone(it)
         }
     }
 
     override fun toString(): String {
-        return "date: $date, localDateTime: $localDateTime, zonedDateTime: $zonedDateTime, timeZone: $timeZone, localToZoned: ${localToZoned()}"
+        return "date: $date" +
+            ", localDateTime: $localDateTime" +
+            ", zonedDateTime: $zonedDateTime" +
+            ", offsetDateTime: $offsetDateTime" +
+            ", zoneId: $zoneId" +
+            ", timeZone: $timeZone" +
+            ", localToZoned: ${localToZoned()}"
     }
 }
 
@@ -89,13 +119,26 @@ class ZonedDateTimeConverter : TypeConverter<String, ZonedDateTime> {
 
     override fun convert(
         str: String?, targetType: Class<ZonedDateTime>?, context: ConversionContext?): Optional<ZonedDateTime> {
-        log.info("Start convert String to LocalDateTime")
+        log.info("Start convert String to ZonedDateTime")
         return Optional.ofNullable(str?.let {
             ZonedDateTime.parse(str, formatter)
         })
     }
 }
 
+/**
+ * JSON 変換で、String 型を ZoneId 型に変換するために必要
+ */
+@Singleton
+class ZoneIdConverter : TypeConverter<String, ZoneId> {
+    private val log = LoggerFactory.getLogger(this.javaClass)
+
+    override fun convert(
+        str: String?, targetType: Class<ZoneId>?, context: ConversionContext?): Optional<ZoneId> {
+        log.info("Start convert String to ZoneId")
+        return Optional.ofNullable(str?.toZoneId())
+    }
+}
 
 /**
  * 文字列を ZoneId に変換
@@ -156,7 +199,7 @@ fun Date.toLocalString(
     formatter: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
 ): String {
     return toInstant()
-        .atZone(zoneId?:ZoneId.of("Asia/Tokyo"))
+        .atZone(zoneId ?: ZoneId.of("Asia/Tokyo"))
         .toLocalDateTime()
         .format(formatter)
 }
